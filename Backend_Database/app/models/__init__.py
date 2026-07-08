6893969bac020da3a27179143ad1a3db82f5c197
Palak Ganwani <palakganwani415@gmail.com>
2026-07-09 02:51:25 +0530

    Import models in __init__.py

---
+++ Backend_Database/app/models/__init__.py
@@ -0,0 +1,7 @@
+from .role import Role
+from .user import User
+from .customer import Customer
+from .store import Store
+from .product import Product
+from .inventory import Inventory
+from .sales_transaction import SalesTransaction
