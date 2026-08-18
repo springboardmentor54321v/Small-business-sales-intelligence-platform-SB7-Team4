cca8f76317cc2dfddce24536306093bab5554400
mrpunithvenkatsain47139 <pnukadas@student.gitam.edu>
2026-08-18 20:40:13 +0530

    Chnages done to present LOCALLY

--- frontend/config/config.py
+++ frontend/config/config.py
@@ -1,5 +1,5 @@
 import os
 
 # Central Config for Frontend APIs
-AUTH_BASE_URL = os.getenv("AUTH_BASE_URL", "https://api-gateway-kwnl.onrender.com")      # Your Gateway link (Port 5000)
-DB_BASE_URL = os.getenv("DB_BASE_URL", "https://small-business-sales-intelligence.onrender.com")     # Teammate's Database (Port 8000)
+AUTH_BASE_URL = os.getenv("AUTH_BASE_URL", "http://localhost:5000")      # Local API Gateway
+DB_BASE_URL = os.getenv("DB_BASE_URL", "http://localhost:8000")     # Local Database Backend
